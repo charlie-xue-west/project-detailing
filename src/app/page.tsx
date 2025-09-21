ceabc318983ac983ee4b361921c454d424c1f5d2
Darkflam3master <x.charlie42@gmail.com>
2025-09-21 13:11:10 -0400

    base skeleton

---
+++ src/app/page.tsx
@@ -0,0 +1,47 @@
+import { MainHeader } from "@/components";
+import { Box, Container } from "@chakra-ui/react";
+
+export default function Home() {
+  const sections = [
+    {
+      id: "sect-about",
+      title: "About",
+      content: (
+        <section style={{ height: "500px", backgroundColor: "red" }}></section>
+      ),
+    },
+    {
+      id: "sect-service",
+      title: "Services",
+      content: (
+        <section
+          style={{ height: "500px", backgroundColor: "yellow" }}
+        ></section>
+      ),
+    },
+    {
+      id: "sect-gallery",
+      title: "Gallery",
+      content: (
+        <section style={{ height: "500px", backgroundColor: "blue" }}></section>
+      ),
+    },
+  ];
+  return (
+    <Box minH="100vh" display="flex" flexDirection="column">
+      <header>
+        <MainHeader />
+      </header>
+      <Box as="main" flex="1" w="100%" bg="white">
+        <Container p={0} maxW={"vw"}>
+          {sections.map((section) => section.content)}
+        </Container>
+      </Box>
+      <footer>
+        <Box bg="black" p={4}>
+          project-detailing.inc © {new Date().getFullYear()}
+        </Box>
+      </footer>
+    </Box>
+  );
+}
---
+++ src/components/layout/MainHeader/MainHeader.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import {
+  Box,
+  Flex,
+  IconButton,
+  useDisclosure,
+  Image,
+  Menu,
+  Portal,
+  Avatar,
+} from "@chakra-ui/react";
+
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { FaSquarePhone, FaInstagram, FaRegEnvelope } from "react-icons/fa6";
+
+import NextImage from "next/image";
+import NextLink from "next/link";
+
+export const MainHeader = () => {
+  const { open, onToggle } = useDisclosure();
+
+  return (
+    <Box
+      bg="black"
+      w="100%"
+      p={4}
+      style={{ position: "sticky", top: 0, zIndex: 100 }}
+    >
+      <Flex direction={{ base: "column" }} gap={4}>
+        <Flex align="center" justify="space-between">
+          <Box hideFrom={"lg"}>
+            <Menu.Root>
+              <Menu.Trigger asChild>
+                <IconButton variant="ghost" size="md" aria-label="Menu">
+                  <HamburgerIcon boxSize={5} />
+                </IconButton>
+              </Menu.Trigger>
+              <Portal>
+                <Menu.Positioner>
+                  <Menu.Content>
+                    <Menu.Item value="new-txt">About</Menu.Item>
+                    <Menu.Item value="new-file">Services</Menu.Item>
+                    <Menu.Item value="new-file">Gallery</Menu.Item>
+                  </Menu.Content>
+                </Menu.Positioner>
+              </Portal>
+            </Menu.Root>
+          </Box>
+
+          <NextLink href="/">
+            <Image asChild>
+              <NextImage
+                src="/project-detailing-white.svg"
+                alt="Project Detailing Logo"
+                width={200}
+                height={60}
+              />
+            </Image>
+          </NextLink>
+
+          {/** User Profile Menu */}
+          <Menu.Root>
+            <Menu.Trigger rounded="full" focusRing="outside">
+              <Avatar.Root size="sm">
+                <Avatar.Fallback name="User Name" />
+                <Avatar.Image src="" />
+              </Avatar.Root>
+            </Menu.Trigger>
+            <Portal>
+              <Menu.Positioner>
+                <Menu.Content>
+                  <Menu.Item value="account">Account</Menu.Item>
+                  <Menu.Item value="settings">Settings</Menu.Item>
+                  <Menu.Item value="logout">Logout</Menu.Item>
+                </Menu.Content>
+              </Menu.Positioner>
+            </Portal>
+          </Menu.Root>
+        </Flex>
+        <Flex
+          flexDirection={{ base: "column", md: "row" }}
+          gap={{ base: 2, md: 8 }}
+          alignItems={"center"}
+          justifyContent={"center"}
+          fontSize={{ base: "sm", md: "md" }}
+        >
+          <Flex alignItems={"center"} justifyContent={"center"} gap={8}>
+            <NextLink
+              href="tel:+19191111111"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "8px",
+                textWrap: "nowrap",
+              }}
+            >
+              <FaSquarePhone size={20} />
+              919-111-1111
+            </NextLink>
+
+            <NextLink
+              href="https://www.instagram.com/project.detailing_/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "8px",
+                textWrap: "nowrap",
+              }}
+            >
+              <FaInstagram size={20} /> project.detailing_
+            </NextLink>
+          </Flex>
+
+          <NextLink
+            href="mailto:Project.detailing919@gmail.com"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+            }}
+          >
+            <FaRegEnvelope size={20} /> project.detailing919@gmail.com
+          </NextLink>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
