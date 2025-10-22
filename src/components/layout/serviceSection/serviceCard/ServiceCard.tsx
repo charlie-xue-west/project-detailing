4eb0eb452bcce71c88bfcfe8f90e04c622dffef8
Darkflam3master <x.charlie42@gmail.com>
2025-10-22 12:05:44 -0400

    color red text test

--- src/components/layout/serviceSection/serviceCard/ServiceCard.tsx
+++ src/components/layout/serviceSection/serviceCard/ServiceCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, List } from "@chakra-ui/react";
+import { Box, Flex, Heading, List, Text } from "@chakra-ui/react";
 type ServiceTypes = {
   type: string;
   descriptions?: string[];
@@ -14,44 +14,48 @@ export const ServiceCard = ({ title, services, price }: ServiceCardProps) => {
   return (
     <Box
       borderWidth={2}
-      borderColor={"#d31920"}
-      px={{ base: 8, md: 10, lg: 16 }}
-      py={{ base: 6, md: 8, lg: 10 }}
+      borderColor={"white"}
+      px={{ base: 8 }}
+      py={{ base: 6 }}
       maxWidth={"800px"}
       height={{ base: "auto", lg: "420px" }}
       width={"100%"}
     >
-      <Heading
-        as={"h3"}
-        size={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
-        color={"#ededed"}
-        mb={8}
-      >
-        {title}
-      </Heading>
+      <Flex flexDirection={"column"} alignItems={"center"} gap={4}>
+        <Heading
+          as={"h3"}
+          size={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
+          color={"#d31920"}
+        >
+          {title}
+        </Heading>
 
-      <Flex
-        direction={{ base: "column", md: "row" }}
-        gap={8}
-        justifyContent={"space-between"}
-      >
-        {services?.map(({ type, descriptions }) => (
-          <Box key={type}>
-            <Heading
-              as={"h4"}
-              size={{ base: "sm", md: "md", lg: "lg", xl: "xl" }}
-            >
-              {type}
-            </Heading>
-            {descriptions && (
-              <List.Root textAlign={"start"}>
-                {descriptions.map((description) => (
-                  <List.Item key={description}>{description}</List.Item>
-                ))}
-              </List.Root>
-            )}
-          </Box>
-        ))}
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          gap={{ base: 4, md: 8 }}
+          justifyContent={"space-between"}
+        >
+          {services?.map(({ type, descriptions, price }) => (
+            <Box key={type}>
+              <Heading
+                as={"h4"}
+                size={{ base: "sm", md: "md", lg: "lg", xl: "xl" }}
+              >
+                {type}
+              </Heading>
+              {descriptions && (
+                <List.Root textAlign={"start"}>
+                  {descriptions.map((description) => (
+                    <List.Item key={description}>{description}</List.Item>
+                  ))}
+                </List.Root>
+              )}
+              {price && <Text color={"#d31920"}>{price}</Text>}
+            </Box>
+          ))}
+        </Flex>
+
+        {price && <Text color={"#d31920"}>{price}</Text>}
       </Flex>
     </Box>
   );
